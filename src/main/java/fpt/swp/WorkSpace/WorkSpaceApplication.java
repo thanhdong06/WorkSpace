aa3b23a78e6025b1289c20dae26cd8debf734bd8
hoquocbao23 <hoquocbao6123@gmail.com>
2024-09-14 08:26:39 +0700

    first commit

---
+++ src/main/java/fpt/swp/WorkSpace/WorkSpaceApplication.java
@@ -0,0 +1,13 @@
+package fpt.swp.WorkSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fpt/swp/WorkSpace/WorkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.swp.WorkSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
